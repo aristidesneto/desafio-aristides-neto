706e4d39b9f6af44598584532dd5b0f69ff380ef
Aristides Neto <aristidesbneto@gmail.com>
2024-02-16 23:18:12 -0300

    Convertendo log da aplicação em formato json

--- comments_api/logger.py
+++ comments_api/logger.py
@@ -1,13 +1,19 @@
 import logging
 import os
 
+from pythonjsonlogger import jsonlogger
+
 logger = logging.getLogger(__name__)
 debug_mode = os.getenv('DEBUG_LEVEL', 'INFO')
 logger.setLevel(debug_mode)
 
 console_handler = logging.StreamHandler()
 console_handler.setLevel(debug_mode)
-console_format = logging.Formatter('%(asctime)s - [%(levelname)s] %(message)s')
+# console_format = logging.Formatter('%(asctime)s - [%(levelname)s] %(message)s')
+console_format = jsonlogger.JsonFormatter(
+    '%(asctime)s %(levelname)s %(message)s'
+)
+
 console_handler.setFormatter(console_format)
 
 logger.addHandler(console_handler)
